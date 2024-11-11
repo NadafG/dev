c3ec9738c86d9ca7ec456097535d9a93f35331a3
NadafG <nadafgaibi@gmail.com>
2024-11-11 12:28:34 +0530

    Docker yml file updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,9 +6,9 @@ const connectDB = async () => {
         if (!process.env.MONGO_URI) {
             throw new Error('MONGO_URI is not defined in the environment variables');
         }
-        const mongoURL = process.env.MONGO_URL || 'mongodb://localhost:27017/zoompoint';
+        const mongoURL = process.env.MONGO_URL || 'mongodb://root:example@mongo:27017/zoompoint?authSource=admin';
         // Establish MongoDB connection
-        await mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: false });
         console.log('MongoDB connected');
     } catch (err) {
         console.error('Error connecting to MongoDB:', err.message);
